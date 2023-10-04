9c8f367485388d1a77394d640e8874659970a560
Cyril Florence <cyrilwaremail@gmail.com>
2023-10-04 18:55:03 +0300

    eslint fixed

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: false,
+    semi: false,
+    endOfLine: "lf",
+    bracketSameLine: true,
+    trailingComma: "none",
+    plugins: ["prettier-plugin-tailwindcss"]
+}
